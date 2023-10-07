0f875fd1352a82fafbbd55d4b16c75dd6f749e58
rahim <991rahim@gmail.com>
2023-10-07 12:17:35 +0600

    9th

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   HOST: "postgres://testdb_7d2g_user:JE276G5YHxd6ecRQxVnJP8AVAold8W9p@dpg-ckf81aenpffc73borij0-a.oregon-postgres.render.com/testdb_7d2g",
-  
+  USER: "postgres",
+  PASSWORD: "postgres",
+  DB: "testdb",
   dialect: "postgres",
   pool: {
     max: 5,
