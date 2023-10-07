635c18568556590f89c124104c5be0c9a3dd7676
rahim <991rahim@gmail.com>
2023-10-07 11:46:30 +0600

    8th Commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HOST: "postgres://testdb_7d2g_user:JE276G5YHxd6ecRQxVnJP8AVAold8W9p@dpg-ckf81aenpffc73borij0-a/testdb_7d2g",
+  HOST: "postgres://testdb_7d2g_user:JE276G5YHxd6ecRQxVnJP8AVAold8W9p@dpg-ckf81aenpffc73borij0-a.oregon-postgres.render.com/testdb_7d2g",
   
   dialect: "postgres",
   pool: {
